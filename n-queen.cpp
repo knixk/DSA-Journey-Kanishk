27bfdb8a4bac48cd861f20f661f7e3d0fa5d60ba
Kanishk Shrivastava <65497298+knixk@users.noreply.github.com>
2023-09-21 21:29:55 +0530

    Update n-queen.cpp

--- n-queen.cpp
+++ n-queen.cpp
@@ -5,33 +5,60 @@ void printAns(int board[][20], int n) {
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			cout << board[i][j];
+			cout << board[i][j] << " ";
 		}
 
 		cout << "\n";
 	}
+}
+
+bool canPlace(int board[][20], int n, int i) {
+
+	// check for top
+	
+	
+	// check for left diag
+
+
+	// check for right diag
+	
 
 }
 
-bool canPlace() {
+void placeNQueens(int board[][20], int n, int i) {
+
+	if (i == n) {
+		return;
+	}
+
+	for (int j = 0; j < n; j++) {
+
+		board[i][j] = 1;
+
+		bool ans = canPlace(board, n, i + 1);
+
+		if (ans) {
+			return true;
+		}
+
+		// go back
+
+		board[i][j] = 0;
+
+	}
+
+	return false;
 
 }
-
-void placeNQueens(int board[][20], int n) {
-
-}
-
 
 int main() {
 
 	int board[20][20] = {0};
 	int n; cin >> n;
 
+	placeNQueens(board, n, 0);
 
-	placeNQueens()
-
-
-
+	printAns(board, n);
 
 	return 0;
 }
