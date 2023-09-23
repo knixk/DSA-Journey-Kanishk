29a5c4a20567a2f8b8746b5661cec4ec23e924f8
Kanishk Shrivastava <65497298+knixk@users.noreply.github.com>
2023-09-23 18:32:17 +0530

    Update n-queen.cpp

--- n-queen.cpp
+++ n-queen.cpp
@@ -5,7 +5,7 @@ void printAns(int board[][20], int n) {
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			cout << board[i][j] << " ";
+			cout << board[i][j] << "  ";
 		}
 
 		cout << "\n";
