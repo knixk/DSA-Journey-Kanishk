6209d49f6d1439276ba47497d982a78c64bf06ca
Kanishk Shrivastava <65497298+knixk@users.noreply.github.com>
2023-09-28 20:44:51 +0530

    Add files via upload

---
+++ sudoku-solver.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+// can you solve sudoku?
+
+int main() {
+
+	int arr[9][9] = {
+	    
+	    {0,  0,  0,  0,  0,  0,  0,  0,  0},
+	    
+	    {0,  0,  0,  0,  0,  0,  0,  0,  0},
+	    
+	    {0,  0,  0,  0,  0,  0,  0,  0,  0},
+	    
+	    {0,  0,  0,  0,  0,  0,  0,  0,  0},
+	    
+	    {0,  0,  0,  0,  0,  0,  0,  0,  0},
+	    
+	    {0,  0,  0,  0,  0,  0,  0,  0,  0},
+	    
+	    {0,  0,  0,  0,  0,  0,  0,  0,  0},
+	    
+	    {0,  0,  0,  0,  0,  0,  0,  0,  0},
+	    
+	    {0,  0,  0,  0,  0,  0,  0,  0,  0},
+	    
+	};
+
+    int n = sizeof(arr) / sizeof(int);
+    
+    // for (int i = 0; i < n; i++) {
+        
+    //     for (int j = 0; j < n; j++) {
+            
+    //         cout << arr[i][j];
+            
+    //     }
+        
+    //     cout << endl;
+        
+    // }
+    
+    cout << n;
+
+	return 0;
+}
+
+
+
