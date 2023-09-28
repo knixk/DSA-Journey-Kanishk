28394d60cb781518f128dc2ea29ba7087728473c
Kanishk Shrivastava <65497298+knixk@users.noreply.github.com>
2023-09-28 21:04:51 +0530

    Add files via upload

--- sudoku-solver.cpp
+++ sudoku-solver.cpp
@@ -5,33 +5,51 @@ using namespace std;
 
 // can you solve sudoku?
 
+bool canBeSolved(int board, int n, int i, int j) {
+
+
+
+}
+
+bool solveSudoku(int board, int n, int i, int j) {
+
+	for (int )
+
+}
+
+
 int main() {
 
-	int arr[9][9] = {
+	int board [ 9 ] [ 9 ] = {
 	    
-	    {0,   0,   0,   0,   0,   0,   0,   0,   0},
+	    {3,   0,   0,   2,   0,   1,   0,   0,   0},
+
+	    {0,   1,   0,   0,   0,   0,   0,   0,   0},
+
+	    {0,   0,   0,   0,   0,   5,   0,   0,   0},
 
 	    {0,   0,   0,   0,   0,   0,   0,   0,   0},
 
-	    {0,   0,   0,   0,   0,   0,   0,   0,   0},
+	    {0,   0,   0,   0,   7,   0,   0,   9,   0},
 
 	    {0,   0,   0,   0,   0,   0,   0,   0,   0},
 
-	    {0,   0,   0,   0,   0,   0,   0,   0,   0},
+	    {0,   9,   0,   0,   0,   0,   0,   0,   0},
 
-	    {0,   0,   0,   0,   0,   0,   0,   0,   0},
+	    {0,   0,   0,   0,   6,   0,   0,   0,   0},
 
-	    {0,   0,   0,   0,   0,   0,   0,   0,   0},
-
-	    {0,   0,   0,   0,   0,   0,   0,   0,   0},
-
-	    {0,   0,   0,   0,   0,   0,   0,   0,   0},
-	    
+	    {0,   0,   0,   3,   0,   0,   0,   2,   0},
 
 	    
 	};
 
-    int n = sizeof(arr) / sizeof(int);
+    int n = sizeof(board) / sizeof(int);
+
+    bool canBeSolved = solveSudoku(board, n);
+
+    if (!canBeSolved) {
+    	cout << "Can't be solved";
+    }
     
     // for (int i = 0; i < n; i++) {
         
@@ -45,7 +63,7 @@ int main() {
         
     // }
     
-    cout << n;
+    // cout << n;
 
 	return 0;
 }
