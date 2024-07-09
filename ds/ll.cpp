482115ad872f8f03ef62de8c7bcc33c56cd77718
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-07-09 22:03:15 +0530

    changes

---
+++ ds/ll.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class node {
+
+	int data;
+	node * next;
+
+public:
+	// public constructor
+
+	node(int data) {
+		// data tied to this instance of object
+		this->data  = data;
+	}
+
+
+}
+
+
+
+int main() {
+
+
+	return 0;
+}
