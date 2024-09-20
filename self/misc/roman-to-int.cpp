b66aacd6e34e966bf7b48caac795c6c008b42523
nefcodes <nefcodes@gmail.com>
2024-09-21 00:15:23 +0530

    changes

---
+++ self/misc/roman-to-int.cpp
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int romanToInt(string s) {
+            unordered_map<string, int> myMap;
+            myMap["I"] = 1;
+            myMap["V"] = 5;
+            myMap["X"] = 10;
+            myMap["L"] = 50;
+            myMap["C"] = 100;
+            myMap["D"] = 500;
+            myMap["M"] = 1000;
+
+    // cout << myMap["M"];
+    }
+};
