baafcd2e19f8396e112a99f2e66f1a67535d24e9
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-02-21 14:25:24 +0530

    changes

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -1,12 +1,12 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+
 int main() {
-
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout); 
-    #endif
-
-	return 0;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
 }
