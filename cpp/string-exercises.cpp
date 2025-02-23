3353704671def2cf6ed6f237cbf35002dde69603
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-02-23 12:50:03 +0530

    updates

---
+++ cpp/string-exercises.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout); 
+    #endif
+
+    // string s;
+    // cin >> s;
+
+    // q. append a number to each character in string..  done
+    // q. reverse a string, find palindrome, find anagram
+    // q. reverse a string with recursion
+    // q. find the most occurence in string
+    // q. implement merge sort.
+    // q. jeez when u will do this?
+    // if u can't solve these everything is useless
+
+    string str = "HelloEveryone";
+    string ans;
+    int count = 1;
+
+    // for (char x : str) {
+    //     // cout << x;
+    //     ans += x;
+    //     char num = count;
+    //     ans += num;
+    //     count++;
+
+    // }
+
+    // cout << ans;
+
+    // use convert to string:
+
+    // for (char x : str) {
+    //     ans += x;
+    //     ans += to_string(count); // Convert number to string
+    //     count++;
+    // }
+
+    // cout << ans;
+
+	return 0;
+}
