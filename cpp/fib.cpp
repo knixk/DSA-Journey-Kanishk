ca6d7ba7757494e6b436c15031cfe77dbc34f5e2
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-02-23 20:05:49 +0530

    added more checks

--- cpp/fib.cpp
+++ cpp/fib.cpp
@@ -5,14 +5,34 @@ using namespace std;
 
 void generateFib(vector <int> & arr, int k, int i) {
 	int n = arr.size();
-	if (n == 0) {
-		arr.push_back(0);
-		arr.push_back(1);
-		++i;
-	}
 
 	if (n == k) {
 		return;
+	}
+
+	// when the arr is empty
+	if (n == 0) {
+
+		if (k == 0) {
+			arr.push_back(-1);
+			return;
+		}
+
+		arr.push_back(0);
+
+
+		if (k == 1) {
+			return;
+		} 
+
+		arr.push_back(1);
+
+		if (k == 2) {
+			return;
+		} 
+
+		++i;
+
 	}
 
 	arr.push_back(arr[i] + arr[i - 1]);
@@ -23,19 +43,25 @@ void generateFib(vector <int> & arr, int k, int i) {
 
 int  main() {
 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout); 
+    #endif
+
 	// can you do k fib nums using recursion?
 
 	vector <int> fib;
-	int k =  10;
+	int k =  0;
 	int i = 0;
 
 
 	generateFib(fib, k, i);
 
+	for (auto x : fib) {
+		cout << x << endl;
+	}
 
-
-
-
+	// cout << "X";
 
 	return 0;
 }
