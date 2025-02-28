2d4422c90d9a1708503adf9f8a484456c7902d5b
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-03-01 00:21:26 +0530

    changes

--- leetcode/longest-common-substring-leetcode.cpp
+++ leetcode/longest-common-substring-leetcode.cpp
@@ -1,31 +1,51 @@
-class Solution {
-public:
+/*
+NOTE: very imp to remember context
 
-    int lengthOfLongestSubstring(string s) {
-        map<char, int> m;
-        int n = s.size();
-        int maxCount = INT_MIN;
+ */
 
-        for (int i = 0; i < n; i++) {
-            int curCount = 0;
-            for (int j = i; j < n; j++) {
-                char curChar = s[j];
-                if (!m[curChar]) {
-                    // i want to add
-                    m[curChar]++;
-                    curCount++;
-                    maxCount = max(curCount, maxCount);
-                } else if (m[j] > 1) {
-                    // i want to clear hashmap and break this j loop
-                    m.clear();
-                    break;
-                }
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int lengthOfLongestSubstring(string s) {
+    map<char, int> m;
+    int n = s.size();
+    int maxCount = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        int curCount = 0;
+        for (int j = i; j < n; j++) {
+            char curChar = s[j];
+            if (!m[curChar]) {
+                // i want to add
+                m[curChar]++;
+                curCount++;
+                maxCount = max(curCount, maxCount);
+            } else if (m[j] > 1) {
+                // i want to clear hashmap and break this j loop
+                m.clear();
+                break;
             }
+
         }
-
-        return maxCount;
-
-
     }
-};
+
+    return maxCount;
+
+
+}
+
+int main() {
+
+        #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout); 
+    #endif
+
+
+    string s = "pwwkew";
+    int ans = lengthOfLongestSubstring(s);
+    cout << ans;
+    return 0;
+}
