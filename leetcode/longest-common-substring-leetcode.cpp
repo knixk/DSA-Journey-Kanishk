3acb44087facf349473c5243687f085925290b18
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-03-01 00:44:16 +0530

    added note

--- leetcode/longest-common-substring-leetcode.cpp
+++ leetcode/longest-common-substring-leetcode.cpp
@@ -1,5 +1,38 @@
 /*
 NOTE: very imp to remember context
+problem name:
+Longest Substring Without Repeating Characters
+link:
+https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+q. Given a string s, find the length of the longest substring 
+without duplicate characters.
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+
+NOTE2: basically we wanted to return the long substring, without repeating
+characters,
+
+we used two loops,
+
+one goes from 0 to n,
+other goes from,
+
+the first loop, til n, (if i = 3, j will start from 3 and so on..)
+
+
+we are using a hashmap,
+if that current character at hashmap doesn't exist,
+we put it inside hashmap, (increment the count)
+
+if it exists or the count is greater than 1,
+we want to clear this hashmap,
+reset the count of substring to 0,
+
+also we maintain a largest count yet.
 
  */
 
@@ -13,18 +46,24 @@ int lengthOfLongestSubstring(string s) {
     int n = s.size();
     int maxCount = INT_MIN;
 
+    if (n == 0) {
+        return 0;
+    }
+
     for (int i = 0; i < n; i++) {
         int curCount = 0;
         for (int j = i; j < n; j++) {
             char curChar = s[j];
+
             if (!m[curChar]) {
                 // i want to add
                 m[curChar]++;
                 curCount++;
                 maxCount = max(curCount, maxCount);
-            } else if (m[j] > 1) {
+            } else if (m[curChar] >= 1) {
                 // i want to clear hashmap and break this j loop
                 m.clear();
+                curCount = 0;
                 break;
             }
 
