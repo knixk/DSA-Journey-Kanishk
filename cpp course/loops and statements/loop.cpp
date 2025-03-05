15eb9ad42bef1ef597ceb9acd2305a5ad4aec511
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-03-05 17:39:46 +0530

    changes

--- cpp course/loops and statements/loop.cpp
+++ cpp course/loops and statements/loop.cpp
@@ -5,9 +5,19 @@ using namespace std;
 /*
 NOTE: damn it seems like i've gotten stuck even here.. jeez
 
+a prime is basically has factors,
+only 
+1 and the number itself,
+
+
+2 is a prime number,
+but two is not a composite number,
+which means it X doesnt has more than two factors..
+
  */
 
 bool checkPrime(int n) {
+
 
 	if (n <= 1) {
 		return false;
@@ -18,11 +28,28 @@ bool checkPrime(int n) {
 		return true;
 	}
 
-	for (int i = 3; i < N / 2; ++i) {
-		for (int j = i)
+	if (n % 2 == 0) {
+		// if it's even, it's not prime
+		return false;
 	}
 
-}
+	for (int i = 2; i < n / 2; ++i) {
+		for (int j = i; i < n / 2; ++j) {
+			// check if there is a factor, except one
+			if (i * j == n) {
+				// there is a factor, so it can't be prime
+				return false;
+			} 
+		}
+	}
+
+	return true;
+
+	// time complexity O((N ^ 2) / 4), worst case
+	// best case O (1);
+	// N / 2 * N * 2
+
+};
 
 int main() {
 
@@ -36,14 +63,23 @@ int main() {
 	int N = 100;
 
 	for (int i = 1; i <= N; i++) {
-		if (i % 2 == 0) {
+
+		if (checkPrime(N)) {
+			cout << i << " is prime" << endl;
+		}
+
+		else if (i % 2 == 0) {
 			// it's an even number
-			cout << i << "is even" << endl;
+			cout << i << " is even" << endl;
 		} else if (i % 2 == 1) {
 			// it's an odd number
-			cout << i << "is odd" << endl;
+			cout << i << " is odd" << endl;
+		} else {
+			cout << i << " is a nice number" << endl;
 		}
 	}
+
+	// cout << checkPrime(4);
 
 	return 0;
 }
