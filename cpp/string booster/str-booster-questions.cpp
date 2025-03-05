92e96b074ce5a67d4212e6ee6efe80301f2068c9
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-03-05 23:31:59 +0530

    changes
    dasd

--- cpp/string booster/str-booster-questions.cpp
+++ cpp/string booster/str-booster-questions.cpp
@@ -14,5 +14,20 @@ int main() {
     freopen("output.txt","w",stdout); 
     #endif
 
+    // Q. read a line split by spaces print each token
+    string s;
+    getline(cin, s);
+
+    string temp;
+
+    stringstream ss(s, temp, ' ');
+
+
+    cout << ss;
+	// works
+
+
+
+
 	return 0;
 }
