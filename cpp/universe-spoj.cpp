43535af843aebaf5cb7c54d7a4e9c0c39ed613dc
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-03-05 14:00:34 +0530

    changes

---
+++ cpp/universe-spoj.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+
+	int t;
+	while (true) {
+		cin >> t;
+		cout << t << endl;
+		if (t == 42) {
+			break;
+		}
+	}
+	
+	return 0;
+}
