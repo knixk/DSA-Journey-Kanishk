17e350eba4bf9cca2f446bea8db208a958441d4a
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-03-08 11:41:47 +0530

    changes

---
+++ problem solving/string boosters/string-questions.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+/* 
+NOTE:
+
+*/
+
+int main() {
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout); 
+    #endif
+
+    // Q. Read a line, split by spaces, and print tokens
+
+    /*
+    Example:
+    // Input: "I love programming"
+    // Output:
+    // I
+    // love
+    // programming
+ 
+     */
+
+    /* 
+    if you use cin, it will only take the consecutive chars,
+    it breaks at the space and a new line
+    */
+
+    string s;
+    cin >> s;
+
+    cout << s;
+
+
+
+
+	return 0;
+}
