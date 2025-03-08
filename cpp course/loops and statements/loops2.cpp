0c3ea9e75ba0253c1b7799978d89e6f80ba721e8
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-03-08 14:09:54 +0530

    changes

--- cpp course/loops and statements/loops2.cpp
+++ cpp course/loops and statements/loops2.cpp
@@ -26,5 +26,46 @@ int main() {
         i++;
     } while (i <= 10); // make sure to use this semicolon
 
+    cout << endl << endl;
+
+    // Q. can u use a while loop to print from 89 to 79?
+
+    int j = 89;
+
+    while (j >= 79) {
+        if (j == 79) {
+            cout << j;
+            break;
+        }
+        cout << j << ", ";
+        j--;
+    }
+
+    // Q. can you use the break and continue keyword?
+
+
+    cout << endl << endl;
+    for (int i = 1; i <= 10; ++i) {
+
+        // if the current number divides 2 completely
+        // we can also write with bitwise say,
+        // (i & 2)   // one for even, 0 for add
+        if (i % 2 == 0) {
+            cout << "number is even\n";
+            continue;
+        }
+
+        // Weird it's not even printing eight
+        // fix this somehow
+        // if (i == 8) {
+        //     cout << " im eight";
+        //     break;
+        // }
+
+        // it won't print the even numbers, only the odd ones right..
+        // also it won't print after 7
+        cout << i << "\n";
+    }
+
 	return 0;
 }
