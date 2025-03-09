035d361ab62591ff2f82a18036528a15e1903da3
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-03-09 12:47:55 +0530

    changes

--- problem solving/hacker-rank/misc/zig-zag-sequence.cpp
+++ problem solving/hacker-rank/misc/zig-zag-sequence.cpp
@@ -25,32 +25,43 @@ loop over from end
 swap the midpoint with position at k,
 
 
-
-
 */
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout); 
+    #endif
     
     int t; cin >> t;
     
     while(t--) {
         int n; cin >> n;
+
+        // make sure to init the arr with a size, otherwise, segmentation err happens
         vector<int> arr;
         vector<int> ans;
         
-        while (n--) {
+        for (int i = 0; i < n; i++) {
             int temp; cin >> temp;
+            // arr[i] = temp;
             arr.push_back(temp);
         }
-        
+
         // do some transformation
-        arr.sort(arr.begin(), arr.end());
+        sort(arr.begin(), arr.end());
+
+        // int size = arr.size();
+
 
         // if u use ( n + 1 ) / 2, it will always find the middle idx
         // basically the mid point, -1 to get the index
-        int k = (arr.size() + 1) / 2;
-        --k;
+        int k = (n + 1) / 2;
+
+        // don't subtract from k, no need
+        // --k;
 
 
         // swap the last character with k, as it has to be the most large element
@@ -66,13 +77,24 @@ int main() {
         }
 
 
-        // set the elements til but excluding k from n - 1
-        for (int j = n - 2; j > k; i++) {
+        // set the elements til and including k
+        for (int j = n - 2; j > k; j--) {
+            ans.push_back(arr[j]);
         }
+
+
+        // ans.push_back(arr[n - 1]);
         
         // print the ans arr     
+        for (auto x : ans) {
+            cout << x << " ";
+        }
+
+        cout << endl;
         
     }
+
+    // cout << " hi";
     
     
     return 0;
