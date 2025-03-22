f3ec10f8e08983c8ca664fabddfc1338ff70cf67
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-03-22 23:56:01 +0530

    changes

---
+++ problem solving/leetcode/container-with-most-water.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        // find the max from the left side
+
+        // find the max from the right side?
+
+
+        // diff between them
+
+        // multiply the number of times seen
+
+
+    // my problem is taking input...
+
+
+    // not solvin questions..
+
+    // then take the essential course or rather take that course...
+
+    // or if u wanna od take the js course i mean
+
+    // it's better u do it in 
+
+    // cuz u have to do it either way
+
+    // sop this is good
+    }
+};
