b7cc5b659ff429b7e0a8371fda6809dd6fe72a75
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-04-07 21:42:09 +0530

    ch

---
+++ problem solving/leetcode/first-and-last-position-in-arr/first-and-last-position-in-arr.cpp
@@ -0,0 +1,38 @@
+
+
+class Solution
+{
+public:
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
+
+        int n = nums.size();
+
+        int first = -1;
+        int second = -1;
+
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == target)
+            {
+                if (first == -1)
+                {
+                    first = i;
+                }
+                else
+                {
+                    second = i;
+                }
+            }
+        }
+
+        // ans[0] = first;
+        // ans[1] = second;
+        ans.push_back(first);
+        ans.push_back(second);
+
+        return ans;
+    }
+};
