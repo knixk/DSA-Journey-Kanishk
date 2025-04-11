87249a14203758936cfcf6a3d0c808ffaef7ab30
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-04-11 18:43:35 +0530

    ch

--- essentials-refresher/merge-sort.cpp
+++ essentials-refresher/merge-sort.cpp
@@ -17,20 +17,33 @@ void merge(vector<int> &arr, int s, int e) {
 		}
 	}
 
+	// copy back any remaning elements from first half if any
 	while (i <= mid) {
 		temp.push_back(arr[i++]);
 	}
 
+	// copy back any remaning elements from second half if any
 	while (j <= e) {
 		temp.push_back(arr[j++]);
 	}
 
-	// becz we didn't know the exact index
+	// becz we don't know the exact index
+
+	/*
+	so what i can see is temp is a new arr,
+	whereas arr is the original array
+
+	we need to copy back the element from temp arr to orignial arr
+	that's why we do this kind of setup,
+	where we increase the k's index with respect to s and e (including of both..)
+	*/
 	int k = 0;
 	for (int idx = s; idx <= e; idx++) {
 		arr[idx] = temp[k++];
 	}
 
+
+	// is this return needed?
 	return;
 }
 
@@ -45,6 +58,9 @@ void mergeSort(vector<int> &arr, int s, int e) {
 
 	mergeSort(arr, s, mid);
 	mergeSort(arr, j, e);
+
+
+	// is thi return needed?
 	return merge(arr, s, e);
 }
 
@@ -66,4 +82,32 @@ int main() {
 	return 0;
 }
 
+/*
+explanation:
+
+basically,
+divide the arr by half
+
+there will be two methods
+
+one is merge sort (where the recurring calls will take plcae)
+next will be merge,
+where the merge will happen when it hits the base case, and returns from the subsequent calls
+
+
+now give the first half to merge sort,
+and then the second half to merge sort,
+
+it will go in the left most tree in the recursion when left
+call finishes it will take the right call..
+
+
+
+*/
+
 // works
+
+
+/*
+uc an never be confident unless u DIY
+*/
