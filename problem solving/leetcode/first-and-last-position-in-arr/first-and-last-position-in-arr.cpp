989a9c1fe7d9e30e0505710b65de331914045f1f
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-04-13 15:16:43 +0530

    ch

--- problem solving/leetcode/first-and-last-position-in-arr/first-and-last-position-in-arr.cpp
+++ problem solving/leetcode/first-and-last-position-in-arr/first-and-last-position-in-arr.cpp
@@ -13,7 +13,7 @@ public:
 
         vector<int> ans;
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) 
         {
             if (nums[i] == target)
             {
---
+++ problem solving/leetcode/valid-parentheses/valid-parenthesis.cpp
@@ -0,0 +1,8 @@
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        
+    }
+};
